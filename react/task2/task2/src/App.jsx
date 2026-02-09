7f6abdabeeae51e78597427a184fac5983719893
New_Lord_8951 <alexandrmaniraki@gmail.com>
2026-02-09 10:48:45 +0300

    Сделано

---
+++ react/task2/task2/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [baza, baza0] = useState(0);
+  const [p, p0] = useState(0);
+  const [m, m0] = useState(0);
+  const [u, u0] = useState(0);
+  const [d, d0] = useState(0);
+  const [del, del0] = useState(0);
+  const but_p = (e) => {
+  };
+
+  return (
+    <div className='baza'>
+      <h1> Пый </h1>
+      <button> СССР
+      </button>
+    </div>
+  )
+}
+
+export default App
